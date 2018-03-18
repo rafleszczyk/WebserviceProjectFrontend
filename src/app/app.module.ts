176fefd01b9318887fc6e7383e78ba1775b22af0
Michał Salamon <salarenko@michas-macbook-pro.home>
2018-03-18 23:12:10 +0100

    Setup | Bootstrap 4, ng-bootstrap, font-awesome 4.7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
-import { AppComponent } from './app.component';
-import { MainTopComponent } from './main-top/main-top.component';
-import { OffertsComponent } from './offerts/offerts.component';
-import { AboutComponent } from './about/about.component';
+import {AppComponent} from './app.component';
+import {MainTopComponent} from './main-top/main-top.component';
+import {OffertsComponent} from './offerts/offerts.component';
+import {AboutComponent} from './about/about.component';
 
 
 @NgModule({
@@ -16,9 +17,11 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
